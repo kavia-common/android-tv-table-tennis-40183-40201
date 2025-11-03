20c783a8843c73c9e3221f9d154e45d779fe7542
Kavia AI <production@kavia.ai>
2025-11-03 17:35:20 +0000

    Checkpoint 1 - cgf2b8133b

---
+++ ping_pong_frontend/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ping_pong_frontend"
+include(":app")
